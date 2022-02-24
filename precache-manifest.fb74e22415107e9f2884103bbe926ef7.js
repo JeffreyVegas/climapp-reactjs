749481eccab0f684eff9059036e4ae6f7813b1e0
jeffreyvegas <jeffreyvegas.web@gmail.com>
2022-02-24 10:32:25 -0500

    Updates

---
+++ precache-manifest.fb74e22415107e9f2884103bbe926ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28e06f41668ff033bbb9f8842fa18048",
+    "url": "/climapp-reactjs/index.html"
+  },
+  {
+    "revision": "21dff2cf558e8ed394a6",
+    "url": "/climapp-reactjs/static/css/main.ae63c931.chunk.css"
+  },
+  {
+    "revision": "4e72b58f803dfbdbe2ef",
+    "url": "/climapp-reactjs/static/js/2.18acbd10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/climapp-reactjs/static/js/2.18acbd10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21dff2cf558e8ed394a6",
+    "url": "/climapp-reactjs/static/js/main.d45cccb1.chunk.js"
+  },
+  {
+    "revision": "1aad4704c5ed1e9407ba",
+    "url": "/climapp-reactjs/static/js/runtime-main.499b5cc3.js"
+  }
+]);
